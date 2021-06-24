3bf777190c946f04d118f1df773a6a708d40875a
Albert Oboh <albertoboh7@gmail.com>
2021-06-24 13:49:24 +0100

    Add menu for logout

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,18 +2,44 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfig: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         Timber.plant(Timber.DebugTree())
 
+        val navController = this.findNavController(R.id.nav_host_fragment)
+
+        // assigning ID of the toolbar to a variable
+        val toolbar = binding.toolbar
+
+        // using toolbar as ActionBar
+        setSupportActionBar(toolbar)
+
+        appBarConfig = AppBarConfiguration(navController.graph)
+
+        NavigationUI.setupWithNavController(toolbar, navController)
+
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = this.findNavController(R.id.nav_host_fragment)
+//        return NavigationUI.navigateUp(navController, appBarConfig)
+//    }
 }
--- app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
@@ -1,12 +1,12 @@
 package com.udacity.shoestore.screens.welcome
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
@@ -26,6 +26,19 @@ class WelcomeFragment : Fragment() {
                 ?.navigate(WelcomeFragmentDirections.actionWelcomeFragmentToInstructionFragment())
         }
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    //inflate menu
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.logout_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
